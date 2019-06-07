50523f30ca9a6c1451422a5910c39e13e6ec58a2
antedeguemon <vammerlo@inf.ufrgs.br>
2019-06-07 13:38:26 -0300

    supervisor

--- lib/udperf.ex
+++ lib/udperf.ex
@@ -1,5 +1,12 @@
 defmodule Udperf do
-  def hello do
-    :world
+  @udp_client_port 3222
+
+  def start(_type, _args) do
+    children = [
+      {UdpFlood.Client, @udp_client_port}
+    ]
+
+    opts = [strategy: :one_for_one, name: A.Supervisor, restart: :transient]
+    Supervisor.start_link(children, opts)
   end
 end
